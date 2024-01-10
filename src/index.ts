924c517e93a3a6879535dbf814b5201166372b4f
Lucas Henrique Calheiros Saraiva Santos <43302698+lucashenriquecss@users.noreply.github.com>
2024-01-10 19:40:24 -0300

    Create:  init project
    Init setup with TS

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// src/index.js
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
